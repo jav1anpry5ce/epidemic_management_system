e78eeb15dd0cd0434b8b20cfb9589db965239b36
Javaughn <javanpryce1@gmail.com>
2022-02-14 17:44:33 -0500

    commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,22 +10,6 @@ module.exports = {
       screens: {
         xs: { max: "550px" },
       },
-      keyframes: {
-        slideIn: {
-          "0%": { transform: "translateX(70%)" },
-          "50%": { transform: "translateX(-10%)" },
-          "100%": { transform: "translateX(0%)" },
-        },
-        slideLeft: {
-          "0%": { transform: "translateX(-150%)" },
-          "50%": { transform: "translateX(30%)" },
-          "100%": { transform: "translateX(0%)" },
-        },
-      },
-      animation: {
-        slideIn: "slideIn 1.5s ease-in-out",
-        slideLeft: "slideLeft 1.5s ease-in-out",
-      },
     },
   },
   plugins: [],
