91ade5da36eaf01f02bd1168493b709d26c3df24
Javaughn <javanpryce1@gmail.com>
2022-02-16 22:06:25 -0500

    commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div className="bg-navBlue-normal flex h-full w-full justify-center">
       <div className="p-1">
         <h3 className="text-center text-base text-white">
-          © 2021 Ministry of Health & Wellness Jamaica.
+          © 2021{" "}
+          <Link className="cursor-pointer hover:underline" to="/accounts/login">
+            Ministry of Health
+          </Link>{" "}
+          & Wellness Jamaica.
         </h3>
         <h3 className="text-center text-base text-white">
           Site developed by Blue Star Technologies Solution.
