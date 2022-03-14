13d682b56bdf9cdfb4f82d3575d6f50425075079
Javaughn <javanpryce1@gmail.com>
2022-03-13 19:16:02 -0500

    commit

--- frontend/src/components/LocationHome.jsx
+++ frontend/src/components/LocationHome.jsx
@@ -26,7 +26,7 @@ export default function LocationHome() {
       data.locationData.moderna_in_stock !== null && {
         name: "Moderna In Stock",
         data: data.locationData.moderna_in_stock,
-        backgroundcolour: "#4f8598",
+        backgroundcolour: "#10496d",
         visible: data.locationData.moderna_in_stock !== null ? true : false,
       },
       data.locationData.jj_in_stock !== null && {
@@ -38,13 +38,13 @@ export default function LocationHome() {
       data.locationData.az_in_stock !== null && {
         name: "AstraZeneca In Stock",
         data: data.locationData.az_in_stock,
-        backgroundcolour: "#4f8598",
+        backgroundcolour: "#10496d",
         visible: data.locationData.az_in_stock !== null ? true : false,
       },
       {
         name: "Pending Appointments",
         data: data.locationData.pending_appointments,
-        backgroundcolour: "#10496d",
+        backgroundcolour: "#4f8598",
         visible: true,
       },
       data.locationData.offer_testing && {
@@ -57,20 +57,20 @@ export default function LocationHome() {
         data.locationData.vaccines_administer !== null && {
           name: "Vaccines Administerd",
           data: data.locationData.vaccines_administer,
-          backgroundcolour: "#10496d",
+          backgroundcolour: "#4f8598",
           visible:
             data.locationData.vaccines_administer !== null ? true : false,
         },
     ];
     return (
-      <div className="container mx-auto flex min-h-[86vh] max-w-7xl">
+      <div className="container mx-auto flex min-h-[86vh] max-w-full">
         <div className="flex w-full flex-1 flex-wrap place-content-center gap-6 px-2 py-4">
           {cardData.map(
             (data) =>
               data.visible && (
                 <div
                   key={data.name}
-                  className="flex h-44 w-96 flex-col justify-between rounded-lg px-4 py-3 pb-12 text-white transition-all duration-300 hover:scale-105 hover:shadow-2xl"
+                  className="flex h-44 w-80 flex-col justify-between rounded-lg px-4 py-3 pb-12 text-white transition-all duration-300 hover:scale-105 hover:shadow-2xl"
                   style={{ backgroundColor: data.backgroundcolour }}
                 >
                   <h3 className="text-center text-2xl font-semibold">
